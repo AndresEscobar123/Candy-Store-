edcc7e8fc7ef61c6b2429e212b32bb26af6c9fb9
rohemer <98710037+rohemer@users.noreply.github.com>
2022-10-27 00:03:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var app = require('./app');
-var mongoose =require ('./ conexBD/conn');
+var mongoose =require ('mongoose');
+
 var port = 4000;
-app.listen(port, () =>{
-    console.log("servidor corriendo ok");
+app.listen(port, () => {
+    console.log("base de datos conectada");
 });
+
+const baseDatos='mongodb://localhost:27017/topcandystore';
+mongoose.connect(baseDatos);
+const database = mongoose.connection;
+
+database.on('error',(error) =>{
+    console.log(error);
+});
+
+database.once('connected',()=>{
+    console.log('BBDD conectada');
+})
